1d1ee363c364e75542df6a845c5da04278f712fb
Arham <arhamreza01@gmail.com>
2025-05-26 00:14:05 +0530

    Update ProjectSide.tsx

--- arham/app/Component/ProjectSide.tsx
+++ arham/app/Component/ProjectSide.tsx
@@ -21,7 +21,7 @@ const ProjectSide: React.FC<ProjectSideProps> = ({ ProjectName, ProjectDetails, 
 
     return (
         <a href={ProjectLink} className="md:h-[30%] md:w-[90%] w-fit p-2 h-fit border-2 hover:border-blue-500 rounded-2xl shrink-0 flex md:flex-row flex-col items-center justify-evenly hover:scale-101 transition duration-150"
-           onClick={handleClick}>
+            onDoubleClick={handleClick}>
             
             {/* Image Container (Only visible when not flipped) */}
             <div className={`md:w-[30%] w-[220] h-fit rounded-2xl flex items-center justify-center ${isFlipped ? "hidden" : ""}`}>
